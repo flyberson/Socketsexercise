c98e31186948dfcdbe9aa75fafac9eeed3b91d81
flyberson <flyberson12@gmail.com>
2017-09-21 09:53:53 +0200

    added GUI and controller

---
+++ src/GUI/GUIController.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GUIController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("GUIRequests.fxml"));
+
+        Scene scene = new Scene(root,800,800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
