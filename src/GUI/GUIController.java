6fd83cfe6a979cd85c93f6b48eb80109f5acd079
flyberson <flyberson12@gmail.com>
2017-09-21 10:55:16 +0200

    updated GUI and controller and runner

--- src/GUI/GUIController.java
+++ src/GUI/GUIController.java
@@ -1,14 +1,17 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class GUIController extends Application {
+    @FXML private Button buttonCount;
 
     public static void main(String[] args) {
         launch(args);
@@ -22,6 +25,10 @@ public class GUIController extends Application {
         Scene scene = new Scene(root,800,800);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+
+
 
     }
 }
